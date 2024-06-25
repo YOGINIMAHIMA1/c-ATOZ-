a82277ebc3cb1bcea16b7caea443e9400ffbcd4b
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-06-25 16:33:43 +0530

    Create structstudent.c

---
+++ structstudent.c
@@ -0,0 +1,16 @@
+Write a C program to define a structure struct Student with members name, age, and marks. Create an array of 3 students, initialize them with values, and print the details of each student.
+
+
+struct Student{
+ char * name;
+ int age;
+int marks;
+};
+int main(){
+struct Student S;
+for(int i=0;i<5;i++){
+printf("%sStudent[i].name
+S.age=21;
+S.marks=89;
+printf("%s",S.name)
+};
