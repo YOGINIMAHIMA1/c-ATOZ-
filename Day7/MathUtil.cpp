486c9bfc047ab0da5538b3906f74f61bbe9f3cd5
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-07-02 12:52:25 +0530

    Create MathUtil.cpp

---
+++ Day7/MathUtil.cpp
@@ -0,0 +1,13 @@
+class MathUtil{
+    static add(int a, int b){
+        cout<<a+b<<endl;
+}
+static subtract(int a, int b){
+    cout<<a-b<<endl;
+}
+static multiply(int a, int b){
+    cout<<a*b<<endl;
+}
+static divide(int a , int b){
+    cout<<a/b<<endl;
+}
